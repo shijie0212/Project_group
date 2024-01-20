09e46f7afffd0d49b727fffb6fbe917899054ef2
shijie0212 <shijie021206@gmail.com>
2024-01-20 22:37:45 +0800

    first commit

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import csv
 
-fp = Path.cwd()/"csv_report"/"cash-on-hand-sgd.csv"
+fp = Path.cwd()/"csv_reports"/"cash-on-hand-sgd.csv"
 
 # read the csv file.
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
@@ -16,5 +16,52 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         #get the driver id, sales, distance, and event type for each record
         #and append to the deliveryRecords list
         cash_on_hand.append([row[0],row[1]])   
-# print(cash_on_hand)
+print(cash_on_hand[0])
         
+
+# def CashonhandFUC():
+#     previous = 0
+#     base_difference = 0 
+#     base_day = 0
+#     for item in cash_on_hand:
+#         new_difference = int(item[1]) - previous 
+#         previous = int(item[1])
+#         if new_difference > base_difference:
+#             base_difference = new_difference 
+#             base_day = item[0]
+#     print(base_day)
+#     print(base_difference)
+#     return (f"{base_day}:{base_difference}")
+
+# print (CashonhandFUC())
+
+# print(cash_on_hand[0][1])
+
+
+
+def CashonhandFUC():
+    previous = 0
+    day=0
+    deficit_list = []
+
+    for item in cash_on_hand:
+        new_difference = int(item[1]) - previous 
+        previous = int(item[1])
+
+        if new_difference < 0:
+            day = item[0]
+            deficit_list.append(new_difference)
+            
+            
+    # Sort deficit_list by deficit amount in ascending order
+    deficit_list.sort()
+    # Get the top 3 highest deficit amounts and their corresponding days
+    top_3_deficits = deficit_list[0:3]
+    for diff in top_3_deficits:
+        difference = abs(diff)
+        print(f'{day},{difference}')
+
+    # Print the top 3 highest deficit amounts and their corresponding days
+    return ""
+
+print(CashonhandFUC())
