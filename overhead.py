b9e51ad1f98bf00934770497d6b9ce92d6efcab5
shijie0212 <shijie021206@gmail.com>
2024-01-21 18:04:45 +0800

    first commit

--- overhead.py
+++ overhead.py
@@ -16,5 +16,34 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         #get the category and overhead for each record
         #and append to the overhead list
         overheads.append([row[0],row[1]])   
-print(overheads)
+# print(overheads)
+
+
+category = []  
+for item in overheads:
+    if item[0] not in category:
+        category.append(item[0])
+print(category)
+percentagelist=[]
+def overheadfuc(cat):
+    total_summary=0
+    for things in overheads:
+        if things[0] == cat:
+            total_summary += float(things[1])
+            percentage = round(total_summary/3,2)
+            percentagelist.append(percentage)
+    return percentage
+
+overalllist = []
+for them in category:
+    overall=overheadfuc(them)
+    overalllist.append(overall)
+    Highest = max(overalllist)
+print(f"[Highest Overhead]{them}:{Highest}")
+    
+print(overalllist)
+
+
+
+
 
