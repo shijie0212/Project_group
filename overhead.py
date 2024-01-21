22ec3dff49e18b1ddeb520f3c318756fdca3d793
shijie0212 <shijie021206@gmail.com>
2024-01-21 17:32:51 +0800

    first commit

--- overhead.py
+++ overhead.py
@@ -1,19 +1,20 @@
 from pathlib import Path
 import csv
 
-fp = Path.cwd()/"csv_reports"/"cash-on-hand-sgd.csv"
+fp = Path.cwd()/"csv_reports"/"overheads-day-90.csv"
 
 # read the csv file.
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader) # skip header
 
-    # create an empty list for delivery record
-    cash_on_hand=[] 
+    # create an empty list for overhead
+    overheads=[] 
 
-    # append delivery record into the deliveryRecords list
+    # append overhead into the overhead list
     for row in reader:
-        #get the driver id, sales, distance, and event type for each record
-        #and append to the deliveryRecords list
-        cash_on_hand.append([row[0],row[1]])   
-print(cash_on_hand[0])
+        #get the category and overhead for each record
+        #and append to the overhead list
+        overheads.append([row[0],row[1]])   
+print(overheads)
+
